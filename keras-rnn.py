f8da4f821aea28252883b3e5cc3447870e20e624
aragaer <aragaer@gmail.com>
2019-06-27 23:48:09 +0300

    Cleanup

--- keras-rnn.py
+++ keras-rnn.py
@@ -40,7 +40,6 @@ def sample(model, seed_ix, n, vocab_size):
     for t in range(n):
         y = model.predict(x)[0]
         ix = np.random.choice(range(vocab_size), 1, p=(y/np.sum(y)).ravel())[0]
-        x = np.zeros((1, 1))
         x[0, 0] = ix
         yield ix
 
@@ -58,13 +57,12 @@ def main(path):
     smooth_loss = -np.log(1.0/data.vocab_size)*seq_length # loss at iteration 0
     smoother = 0.001 * (len(data.data) // seq_length) * epochs
     X = np.ndarray((len(data.data)-1, 1), dtype=int)
-    y = np.ndarray((len(data.data)-1, data.vocab_size), dtype=int)
+    y = np.zeros((len(data.data)-1, data.vocab_size), dtype=int)
     for t, (i, o) in enumerate(zip(data.data, data.data[1:])):
         if t % 100 == 0:
             print(t, '/', len(data.data), end='\r')
             sys.stdout.flush()
         X[t, 0] = data.char_to_ix[i]
-        y[t, :] = np.zeros(data.vocab_size)
         y[t, data.char_to_ix[o]] = 1
     while True:
         model.reset_states()
