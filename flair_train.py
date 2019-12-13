04d6521e0bee0fa91a07f70c71b16dd91444321e
aragaer <aragaer@gmail.com>
2019-12-14 01:21:36 +0300

    Fix model paths

--- flair_train.py
+++ flair_train.py
@@ -14,8 +14,8 @@ def main():
     mappings = human2pa.train.make_vocab(tlk_corpus, path=path)
     flair_corpus = human2pa.train.make_corpus(tlk_corpus, path=path)
 
-    human2pa.train.train_model(flair_corpus, mappings, 'tlk-forward', forward=True)
-    human2pa.train.train_model(flair_corpus, mappings, 'tlk-backward', forward=False)
+    human2pa.train.train_model(flair_corpus, mappings, path / 'tlk-forward', forward=True)
+    human2pa.train.train_model(flair_corpus, mappings, path / 'tlk-backward', forward=False)
 
 
 if __name__ == '__main__':
